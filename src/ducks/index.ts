acfd2b23dffe674898d2f4fd4167e450d4b8a341
mmitou <mitou.interface@gmail.com>
2020-02-22 18:05:22 +0900

    refactored

--- src/ducks/index.ts
+++ src/ducks/index.ts
@@ -1,16 +1,19 @@
 import { createStore, combineReducers } from 'redux';
 import * as auth0 from './auth0';
 
-export type AppState = {
+// State
+export type State = {
 	auth0: auth0.State;
 };
 
+// store
 export const store = createStore (
-	combineReducers<AppState>({
+	combineReducers<State>({
 		auth0: auth0.reducer
 	})
 );
 
+// action
 export const action = {
 	auth0: { ...auth0.action }
 };
--- src/sagas/auth0/index.ts
+++ src/sagas/auth0/index.ts
@@ -3,26 +3,26 @@ import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { bindAsyncAction } from 'typescript-fsa-redux-saga';
 import { SagaIterator } from 'redux-saga';
-import { action, Auth0Client } from 'ducks';
+import * as ducks from 'ducks';
 import createAuth0Client from '@auth0/auth0-spa-js';
 
-const actionCreator = actionCreatorFactory();
-
 export type Auth0ClientOptions = {
-	domain:string,
-	client_id:string,
+	domain: string,
+	client_id: string,
 	redirect_uri: string
 };
 
 // action
-export const createAuth0ClientAction =
-	actionCreator.async<Auth0ClientOptions, Auth0Client, Error>('auth0/CREATE_AUTH0CLIENT');
-export const handleRedirectCallbackAction =
-	actionCreator.async<void, void, Error>('auth0/HANDLE_REDIRECT_CALLBACK');
+const actionCreator = actionCreatorFactory();
+export const action = {
+	createAuth0Client: actionCreator.async<Auth0ClientOptions, ducks.Auth0Client, Error>('auth0/CREATE_AUTH0CLIENT'),
+	handleRedirectCallback: actionCreator.async<void, void, Error>('auth0/HANDLE_REDIRECT_CALLBACK'),
+};
 
+// saga
 function* createAuth0ClientWorker(opt: Auth0ClientOptions) {
 	const client = yield call(createAuth0Client, opt);
-	yield put(action.auth0.setAuth0Client(client));
+	yield put(ducks.action.auth0.setAuth0Client(client));
 }
 
 function* handleRedirectCallbackWorker() {
@@ -36,27 +36,27 @@ function* handleRedirectCallbackWorker() {
 	console.log(_redirectLoginResult);
 
 	const user = yield call([client, 'getUser']);
-	yield put(action.auth0.setAuth0User(user));
+	yield put(ducks.action.auth0.setAuth0User(user));
 }
 
 const boundCreateAuth0ClientWorker =
-	bindAsyncAction(createAuth0ClientAction, {skipStartedAction: true})(createAuth0ClientWorker);
+	bindAsyncAction(action.createAuth0Client, {skipStartedAction: true})(createAuth0ClientWorker);
 const boundHandleRedirectCallbackWorker =
-	bindAsyncAction(handleRedirectCallbackAction, {skipStartedAction: true})(handleRedirectCallbackWorker);
+	bindAsyncAction(action.handleRedirectCallback, {skipStartedAction: true})(handleRedirectCallbackWorker);
 
 function* createAuth0Handler(): SagaIterator {
-	yield takeLatest(createAuth0ClientAction.started, function* ({payload}) {
+	yield takeLatest(action.createAuth0Client.started, function* ({payload}) {
 		yield call(boundCreateAuth0ClientWorker, payload); 
 	});
 }
  
 function* redirectCallbackHandler(): SagaIterator {
-	yield takeLatest(handleRedirectCallbackAction.started, function* ({payload}) {
+	yield takeLatest(action.handleRedirectCallback.started, function* ({payload}) {
 		yield call(boundHandleRedirectCallbackWorker, payload);
 	});
 }
 
-function* auth0Saga(): SagaIterator {
+export function* rootSaga(): SagaIterator {
 	yield fork(createAuth0Handler);
 	yield fork(redirectCallbackHandler);
 }
