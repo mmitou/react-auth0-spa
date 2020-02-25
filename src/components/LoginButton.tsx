9b4ba17caa9cbed5e207aa54ff40cc96a44898c5
mmitou <mitou.interface@gmail.com>
2020-02-25 16:41:00 +0900

    refactor

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Dropdown, Button } from 'react-bootstrap';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { action } from 'sagas';
+import { State, User } from 'ducks';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const LoginButton: React.FC = () => {
+	const user = useSelector<State, User | null >(state => state.auth0.user, shallowEqual);
+	const history = useHistory();
+	const dispatch = useDispatch();	
+	const login = () => {
+		dispatch(action.auth0.createAuth0Client.started());
+	};
 
-	return (
-		<button>
-			login
-		</button>
-	);
-};
+	if (user == null) {
+		return (<Button className="px-5" onClick={login}>Log in</Button>);
+	} else {
+		return (
+			<Dropdown>
+				<Dropdown.Toggle id="profileDropDown" variant="light">
+					<img src={user.picture} alt="Profile" className="nav-user-profile rounded-circle" width="50" />
+				</Dropdown.Toggle>
 
+				<Dropdown.Menu>
+					<Dropdown.Header> {user.name} </Dropdown.Header>
+					<Dropdown.Item onClick={()=>history.push("/about")}>
+						<FontAwesomeIcon icon="user" className="mr-2" /> About
+					</Dropdown.Item>
+					<Dropdown.Divider />
+					<Dropdown.Item onClick={() => alert("log out")}>
+						<FontAwesomeIcon icon="power-off" className="mr-2" /> Log out
+					</Dropdown.Item>
+
+				</Dropdown.Menu>
+			</Dropdown>
+		);
+	}
+}
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
-import { Dropdown, Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import { useHistory } from 'react-router-dom';
-import { action } from 'sagas';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { State, User } from 'ducks';
+import { LoginButton } from './LoginButton';
 
 export const NavigationBar = () => {
-	const user = useSelector<State, User | null >(state => state.auth0.user, shallowEqual);
-	const history = useHistory();
-	const dispatch = useDispatch();	
-	const login = () => {
-		dispatch(action.auth0.createAuth0Client.started());
-	};
-
 	return (
 		<Navbar bg="light" className="py-3">
 			<Container>
-				<Navbar.Brand href="/">Auth0 Sample</Navbar.Brand>
+				<LinkContainer to="/">
+					<Navbar.Brand>Auth0 Sample</Navbar.Brand>
+				</LinkContainer>
 				<Navbar.Toggle aria-controls="app-navbar" />
 				<Navbar.Collapse id="app-navbar">
 					<Nav className="mr-auto">
@@ -34,40 +25,9 @@ export const NavigationBar = () => {
 						</Nav.Item>
 					</Nav>
 					<Nav>
-						{ (user != null) ?
-							(
-							  <Nav.Item>
-								  <Dropdown>
-								 		<Dropdown.Toggle id="profileDropDown" variant="light">
-								  		<img src={user.picture} alt="Profile" className="nav-user-profile rounded-circle"
-												width="50" />
-										</Dropdown.Toggle>
-
-										<Dropdown.Menu>
-											<Dropdown.Header>
-												{user.name}
-											</Dropdown.Header>
-											<Dropdown.Item onClick={()=>history.push("/about")}>
-								        <FontAwesomeIcon icon="user" className="mr-2" /> About
-								    	</Dropdown.Item>
-
-									  	<Dropdown.Divider />
-	
-									  	<Dropdown.Item onClick={() => alert("log out")}>
-										  	<FontAwesomeIcon icon="power-off" className="mr-2" /> Log out
-									  	</Dropdown.Item>
-
-										</Dropdown.Menu>
-								  </Dropdown>
-							  </Nav.Item>
-							)
-							:
-							(
-								<Nav.Item>
-									<Button className="px-5" onClick={login}>Log in</Button>
-								</Nav.Item>
-							)
-						}
+						<Nav.Item>
+							<LoginButton />
+						</Nav.Item>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
